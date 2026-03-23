e3e92253cd3faa1dca482819f92848f966d9e707
djanenosa28 <kaliankontol1@gmail.com>
2026-03-23 23:02:41 +0700

    UI

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
 });
 
 // ─── Middleware ───────────────────────────────────────────────────────────────
-app.use(cors({ origin: false })); // disable CORS for same-origin only
+app.use(cors());
 app.use(express.json({ limit: '512kb' }));
 app.use(express.urlencoded({ extended: true, limit: '512kb' }));
 app.use(express.static(path.join(__dirname, 'public')));
